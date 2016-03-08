052f5a4e562f9c81c2cc6d5a12af7df8c70eb7cc
josephquested <josephquested@gmail.com>
2016-03-09 10:29:45 +1300

    Fix deployment issues

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import parser from 'body-parser'
 import dotenv from 'dotenv'
 import routes from './routes'
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 dotenv.load()
 
